0dc56402e8a9e8e175d83faa89ebdaf5eb5560bc
RawBoeuf <fencedfruit@gmail.com>
2021-11-30 03:43:26 -0800

    finished substring_norepeat

--- Medium/substring_norepeat.cpp
+++ Medium/substring_norepeat.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <unordered_map>
 #include <iostream>
 
 using namespace std;
@@ -6,6 +7,23 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        int ans = 0;
+        unordered_map<char, int> umap;
+        for(int j = 0, i = 0; j < s.length(); j++)
+        {
+            if(umap.count(s[j]))
+            {
+                i = max(umap[s[j]], i);
+            }
+            ans = max(ans, j - i + 1);
+            umap[s[j]] = j+1;
+        }
+        return ans;
     }
 };
+
+/*
+LeetCode Statistics:
+8 ms - Beats 64.54% of Users
+10.7 mb - Beats 56.25% of Users
+*/
